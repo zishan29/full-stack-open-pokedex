ce2f6f9a9ea81b56095d5cc829b187efdb3376f9
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-08-01 22:44:14 +0530

    Fix PokemonPage next link error

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import propTypes from "prop-types";
 
 const ErrorMessage = ({ error }) => (
   <div data-testid="error">An error occured: {error.toString()}</div>
 );
 
 ErrorMessage.propTypes = {
-  error: Error,
+  error: propTypes.object.isRequired,
 };
 
 export default ErrorMessage;
